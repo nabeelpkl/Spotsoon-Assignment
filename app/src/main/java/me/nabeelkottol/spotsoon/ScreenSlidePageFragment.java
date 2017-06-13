79ad39f58d39f3be6e8e9f547f67562bfe98d6d3
Nabeel K <nabeel.kottol@gmail.com>
2017-06-13 06:52:30 +0530

    perfomance issue fix

--- app/src/main/java/me/nabeelkottol/spotsoon/ScreenSlidePageFragment.java
+++ app/src/main/java/me/nabeelkottol/spotsoon/ScreenSlidePageFragment.java
@@ -6,18 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import static me.nabeelkottol.spotsoon.ImageHelper.decodeSampledBitmapFromResource;
 
 /**
  * Created by nabeelkottol on 12/06/17.
  */
 
 public class ScreenSlidePageFragment extends Fragment {
+  ImageView backgroundView;
+
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    ViewGroup rootView = (ViewGroup) inflater.inflate(
-        R.layout.fragment_screen_slide_page, container, false);
-
+    ViewGroup rootView =
+        (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+    backgroundView = (ImageView) rootView.findViewById(R.id.background_image);
+    backgroundView.setImageBitmap(
+        decodeSampledBitmapFromResource(getResources(), R.drawable.akcent_cover_art, 250, 150));
     return rootView;
   }
 }
